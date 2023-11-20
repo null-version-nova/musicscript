318b1549ad8c65eae1e29d0dbe88cf99a746cae2
null-version-nova <ethcoyle@gmail.com>
2023-11-20 14:38:14 -0600

    working on fixing forge

--- forge/src/main/kotlin/org/nullversionnova/musicscript/forge/MusicScriptForge.kt
+++ forge/src/main/kotlin/org/nullversionnova/musicscript/forge/MusicScriptForge.kt
@@ -3,6 +3,7 @@ package org.nullversionnova.musicscript.forge
 import dev.architectury.platform.forge.EventBuses
 import net.minecraftforge.fml.common.Mod
 import org.nullversionnova.musicscript.MusicScript
+import org.nullversionnova.musicscript.script.PythonScriptManager
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(MusicScript.MOD_ID)
@@ -11,5 +12,6 @@ object MusicScriptForge {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(MusicScript.MOD_ID, MOD_BUS)
         MusicScript.init()
+        PythonScriptManager.init()
     }
 }
